a4a0b67ee0631f6a1bdf70c5491d5857d825ae58
calmera <daan.gerits+code@gmail.com>
2017-12-11 15:02:57 +0100

    typo. stupid one. really.

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-#/bin/sh
+#/bin/bash
 
 ## -- check if the environment variables have been set.
-if [ ! -z $DP_LIBRARIES ]; then {
+if [ ! -z $DP_LIBRARIES ]; then
     npm i ${DP_LIBRARIES}
-}
+fi
 
 cd  /usr/src/app
 
